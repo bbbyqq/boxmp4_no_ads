acdc20241d72201ba855b999b52d7356599ef54b
bbbyqq <2287532812@qq.com>
2022-09-29 09:45:39 +0800

    修改定时器

--- tampermonkey.js
+++ tampermonkey.js
@@ -8,27 +8,27 @@
 // @match        *://www.ysgc.cc/*
 // @match        *://boxmp4.com/*
 // @match        *://ysgc.cc/*
+// @match        *://www.a2mu.com/*
 // @license      bbbyqq
 // ==/UserScript==
 
 (function () {
   'use strict'
 
-  let flag = false
-  let t = setInterval(() => {
-    if (flag) {
-      clearInterval(t)
-    } else {
-      const box = document.querySelector('#HMRichBox')
-      const left = document.querySelector('#HMcoupletDivleft')
-      const right = document.querySelector('#HMcoupletDivright')
-      if (box && left && right) {
-        box.remove()
-        left.remove()
-        right.remove()
-        flag = true
-      }
-    }
+  setInterval(() => {
+    const box = document.querySelector('#HMRichBox')
+    const left = document.querySelector('#HMcoupletDivleft')
+    const right = document.querySelector('#HMcoupletDivright')
+    const bfad = document.querySelector('#bfad')
+    const xqad = document.querySelector('#xqad')
+    const adv = document.querySelector('#adv_wrap_hh')
+
+    box?.remove()
+    left?.remove()
+    right?.remove()
+    bfad?.remove()
+    xqad?.remove()
+    adv?.remove()
   }, 1)
 
 })();
